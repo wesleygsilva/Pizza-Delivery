ced80cad345f896529e7410ce32517213fe2cfc1
Wesley Gomes <gomes.wesley@outlook.com.br>
2022-04-10 20:25:51 -0300

    implementation cupom's module

--- cupons.js
+++
@@ -1,4 +0,0 @@
-//cupom available for pizzas
-let cupomJson = [
-    {id:1, promocode:'VALE10', discounttype:'percent', value:10, description:'10% de desconto em compras acima de R$50,00', initialDate:'2022-04-10', finalDate:'2022-12-31', valid: true}
-];
